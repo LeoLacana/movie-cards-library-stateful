94f248e3418171c6f2d44df61bb2899279862bc3
Luanderson Santos <lu-anderson1@hotmail.com>
2021-05-31 11:48:23 -0400

    Tests and lint breaking

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render(){
+        return <h1>Olá</h1>
+    }
+}
+
+export default SearchBar
