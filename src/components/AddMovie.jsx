da77a1971d418b584674b353891c17efdaeb182e
LeoLacana <wilson17414@gmail.com>
2021-06-04 21:05:00 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+    };
+
+    this.textWritten = this.textWritten.bind(this);
+  }
+
+  textWritten({ target }) {
+    this.setState({ title: target.value });
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            onChange={ this.textWritten }
+            type="text"
+            data-testid="title-input"
+            value={ title }
+          />
+        </label>
         { onClick }
       </form>
     );
