603cde539bda412548a6b27f698d0597fbdf608e
LeoLacana <wilson17414@gmail.com>
2021-06-08 19:33:52 -0300

    requisito 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <div />
+    );
+  }
+}
+
+/* MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+}; */
+
+export default MovieLibrary;
