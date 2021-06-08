ab352a737d3771ca7686513ae28015f1c605bfc5
LeoLacana <wilson17414@gmail.com>
2021-06-07 23:39:52 -0300

    refatorado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -7,15 +7,54 @@ import StorylineInput from './StorylineInput';
 import RatingInput from './RatingInput';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+    };
+
+    this.textWrittenTitle = this.textWrittenTitle.bind(this);
+    this.textWrittenSubtitle = this.textWrittenSubtitle.bind(this);
+    this.imagePathAdd = this.imagePathAdd.bind(this);
+    this.textAreaWritten = this.textAreaWritten.bind(this);
+    this.textRating = this.textRating.bind(this);
+  }
+
+  textWrittenTitle({ target }) {
+    this.setState({ title: target.value });
+  }
+
+  textWrittenSubtitle({ target }) {
+    this.setState({ subtitle: target.value });
+  }
+
+  imagePathAdd({ target }) {
+    this.setState({ imagePath: target.value });
+  }
+
+  textAreaWritten({ target }) {
+    this.setState({ storyline: target.value });
+  }
+
+  textRating({ target }) {
+    this.setState({ rating: target.value });
+  }
+
   render() {
+    const { title, subtitle, imagePath, storyline, rating } = this.state;
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        <TitleInput />
-        <SubtitleInput />
-        <ImageInput />
-        <StorylineInput />
-        <RatingInput />
+        <TitleInput funcValue={ this.textWrittenTitle } stateValue={ title } />
+        <SubtitleInput funcValue={ this.textWrittenSubtitle } stateValue={ subtitle } />
+        <ImageInput funcValue={ this.imagePathAdd } stateValue={ imagePath } />
+        <StorylineInput funcValue={ this.textAreaWritten } stateValue={ storyline } />
+        <RatingInput funcValue={ this.textRating } stateValue={ rating } />
         { onClick }
       </form>
     );
